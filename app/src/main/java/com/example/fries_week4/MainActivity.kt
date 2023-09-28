24d0080cb98c3b5e283825922f464397cd401e79
Evan Fries <evanfries@gwu.edu>
2023-09-27 22:22:04 -0400

    main page

--- app/src/main/java/com/example/fries_week4/MainActivity.kt
+++ app/src/main/java/com/example/fries_week4/MainActivity.kt
@@ -13,32 +13,23 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myList: ListView
-    private lateinit var myButton: Button
-    private lateinit var countryClicked : String
-
+    private lateinit var mapButton: Button
+    private lateinit var headlinesButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myList = findViewById(R.id.listView)
-        myButton = findViewById(R.id.button)
+        mapButton = findViewById(R.id.mapButton)
+        headlinesButton = findViewById(R.id.headlinesButton)
 
         val sharedPrefs  = getSharedPreferences("savedStuff", MODE_PRIVATE)
 
-        var countryList = resources.getStringArray(R.array.countries)
-        var arrayAdapter= ArrayAdapter(this, android.R.layout.simple_list_item_1, countryList)
-        myList.adapter = arrayAdapter
+        mapButton.setOnClickListener(View.OnClickListener { // Create an Intent to switch to the target activity
+            val intent = Intent(this@MainActivity, MapsActivity::class.java)
+            startActivity(intent)
+        })
 
-        myList.setOnItemClickListener { parent, view, position, id ->
-            countryClicked = parent.getItemAtPosition(position).toString()
-            Toast.makeText(this, "You selected $countryClicked", Toast.LENGTH_LONG).show()
-            Log.d("country", "position $countryClicked")
-        }
-
-        myButton.setOnClickListener(View.OnClickListener { // Create an Intent to switch to the target activity
-            val country : String = countryClicked
-            sharedPrefs.edit().putString("COUNTRY", countryClicked).apply()
-
+        headlinesButton.setOnClickListener(View.OnClickListener { // Create an Intent to switch to the target activity
             val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
         })
